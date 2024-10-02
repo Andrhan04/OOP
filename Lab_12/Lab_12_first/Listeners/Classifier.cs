146c5de5bb360b71eba373bf871af9cb8bae6b87
Andrhan04 <andrham2004@yandex.ru>
2024-10-02 10:45:06 +0800

    Add files via upload

---
+++ Lab_12/Lab_12_first/Listeners/Classifier.cs
@@ -0,0 +1,47 @@
+﻿using Lab_12_first;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12_first.Listeners
+{
+    internal class Classifier : Listener
+    {
+        private List<string> asteroid = new List<string>();
+        public override void MyData()
+        {
+            Console.WriteLine(GetType());
+            int i = 0;
+            foreach (var item in asteroid)
+            {
+                Console.WriteLine($"{++i}) INFO {item}");
+            }
+        }
+
+        public override void Take(Asteroid a)
+        {
+            if (a.Mass > 200)
+            {
+                asteroid.Add("Heavy");
+            }
+            else
+            {
+                if(a.Speed > 40)
+                {
+                    asteroid.Add("Fast");
+                }
+                else
+                {
+                    asteroid.Add((a.Squeare > 100)?"Big":"Normal");
+                }
+            }
+        }
+
+        public override void Clear()
+        {
+            asteroid = new List<string>();
+        }
+    }
+}
